5fca56b0120f359ea4af4e1ccf24f043912036bd
Wim Van Laer <van.laer.wim@gmail.com>
2023-07-03 22:32:47 +0200

    removed windows and linux

---
+++ admin_app/lib/shared/dependency_management/feature.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:get_it/get_it.dart';
+
+abstract class Feature {
+  const Feature();
+
+  List<Type> get dependencies => const [];
+
+  void registerTypes(GetIt getIt) {}
+
+  Future<void> install(GetIt getIt) => Future.value();
+
+  FutureOr<void> dispose() {}
+
+  @override
+  String toString() => runtimeType.toString();
+}
+
+abstract class FeatureWithSubscriptions extends Feature {
+  FeatureWithSubscriptions();
+
+  List<StreamSubscription> subscriptions = [];
+
+  @override
+  List<Type> get dependencies => const [];
+
+  @override
+  void registerTypes(GetIt getIt) {}
+
+  @override
+  Future<void> install(GetIt getIt) => Future.value();
+
+  @override
+  FutureOr<void> dispose() async {
+    await Future.wait(
+      subscriptions.map((subscription) => subscription.cancel()),
+    );
+  }
+}
---
+++ admin_app/lib/shared/firebase/models/spotify_config.dart
@@ -0,0 +1,18 @@
+import 'package:admin_app/shared/json_extensions.dart';
+
+class SpotifyConfig {
+  const SpotifyConfig({
+    required this.clientId,
+    required this.clientSecret,
+  });
+
+  factory SpotifyConfig.fromFirebase(Map<String, dynamic> remoteValue) {
+    return SpotifyConfig(
+      clientId: remoteValue.get('clientId'),
+      clientSecret: remoteValue.get('clientSecret'),
+    );
+  }
+
+  final String clientSecret;
+  final String clientId;
+}
